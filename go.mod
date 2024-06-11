bcb6d1ba465991b0c0ef6a8fec6b5c46389025e5
Thomas <t.collet974@outlook.fr>
2024-06-11 19:12:54 +0200

    new structure adding js

--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/Thomas97460/Scrapper_Moodle_Video
 
 go 1.22.4
+
+require github.com/fatih/color v1.17.0
+
+require (
+	github.com/PuerkitoBio/goquery v1.9.2 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
--- pkg/scrapper/scrapper.go
+++ pkg/scrapper/scrapper.go
@@ -1,25 +1,33 @@
 package scrapper
 
 import (
-	"io/ioutil"
+	"io"
 	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-func SendRequest(targetUrl string, cookieValue string) (string, error) {
+func SendRequest(target_url string, cookie_moodle_session string, cookie_moodle_id string) (string, error) {
 	// Créer une nouvelle requête HTTP
-	req, err := http.NewRequest("GET", targetUrl, nil)
+	req, err := http.NewRequest("GET", target_url, nil)
 	if err != nil {
 		return "", err
 	}
 
-	// Créer un nouveau cookie
-	cookie := &http.Cookie{
+	// Créer 2 nouveaux cookies
+	cookie1 := &http.Cookie{
 		Name:  "MoodleSession",
-		Value: cookieValue,
+		Value: cookie_moodle_session,
+	}
+	cookie2 := &http.Cookie{
+		Name:  "MOODLEID1_",
+		Value: cookie_moodle_id,
 	}
 
-	// Ajouter le cookie à la requête
-	req.AddCookie(cookie)
+	// Ajouter les cookies à la requête
+	req.AddCookie(cookie1)
+	req.AddCookie(cookie2)
 
 	// Envoyer la requête
 	client := &http.Client{}
@@ -30,10 +38,27 @@ func SendRequest(targetUrl string, cookieValue string) (string, error) {
 	defer resp.Body.Close()
 
 	// Lire le corps de la réponse
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return "", err
 	}
 
 	return string(body), nil
+}
+
+func IsConnected(body string) bool {
+	return strings.Contains(body, "Déconnexion")
+}
+
+func GetName(body string) (string, error) {
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(body))
+
+	if err != nil {
+		panic(err)
+	}
+	var name string
+	doc.Find(".logininfo a:first-child").Each(func(i int, s *goquery.Selection) {
+		name = s.Text()
+	})
+	return name, nil
 }
